879acc3fc7ccdef3f3f1f36d0e20b2b8d0bc6a7f
sebastian.cornejo.silva@gmail.com <sebastian.cornejo.silva@gmail.com>
2023-02-09 23:52:08 -0300

    Se instala typescript agregan archivos de configuracion TS

--- src/hocs/layouts/FullWidthLayout.js
+++ src/hocs/layouts/FullWidthLayout.js
@@ -1,8 +1,14 @@
 import { connect } from "react-redux"
+import Footer from "../../components/navigation/Footer"
+import Navbar from "../../components/navigation/Navbar"
 
 const FullWidthLayout = ({children}) => {
     return(
+        <>
+        <Navbar/>
         {children}
+        <Footer/>
+        </>
     )
 }
 const mapStateToProps = state =>({
