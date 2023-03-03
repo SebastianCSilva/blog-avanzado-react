6f4063f975511f3f9fd095affe3e8bd5a2fdd405
sebastian.cornejo.silva@gmail.com <sebastian.cornejo.silva@gmail.com>
2023-03-02 23:32:11 -0300

    Se agrega las urls de las categorias y post filtrados por categorias

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -10,11 +10,28 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from .serializers import PostSerializer
 from .pagination import SmallSetPagination, MediumSetPagination, LargeSetPagination
+from .models import Category
 
 class BlogListView(APIView):
     def get(self, request, format=None):
         if Post.postobjects.all().exists():
             posts = Post.postobjects.all()
+
+            paginator = SmallSetPagination()
+            results = paginator.paginate_queryset(posts, request)
+            serializer = PostSerializer(results, many=True)
+
+            return paginator.get_paginated_response({'posts': serializer.data})
+        else:
+            return Response({'error': 'No posts found'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class BlogListCategoryView(APIView):
+    def get(self, request, category_id, format=None):
+        if Post.postobjects.all().exists():
+
+            category = Category.objects.get(id=category_id)
+
+            posts = Post.postobjects.all().filter(category=category)
 
             paginator = SmallSetPagination()
             results = paginator.paginate_queryset(posts, request)
--- src/redux/actions/blog.js
+++ src/redux/actions/blog.js
@@ -6,6 +6,8 @@ import {
     GET_BLOG_FAIL,
     GET_BLOG_PAGINATION_RESULTS_SUCCESS,
     GET_BLOG_PAGINATION_RESULTS_FAIL,
+    GET_BLOG_LIST_CATEGORIES_SUCCESS,
+    GET_BLOG_LIST_CATEGORIES_FAIL,
 } from "./types";
 
 export const get_blog_list = () => async dispatch => {
@@ -94,6 +96,66 @@ export const get_blog = (slug) => async dispatch => {
     }catch{
         dispatch({
                 type: GET_BLOG_PAGINATION_RESULTS_FAIL
+            });
+    }
+}
+
+export const get_blog_list_category = (category_id) => async dispatch => {
+
+    const config = {
+        headers: {
+            'Accept': 'application/json'
+        }
+    };
+
+    try{
+        const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/blog/category/${category_id}`, config)
+
+
+        if (res.status === 200){
+            dispatch({
+                type: GET_BLOG_LIST_CATEGORIES_SUCCESS,
+                payload: res.data
+            });
+        } else {
+            dispatch({
+                type: GET_BLOG_LIST_CATEGORIES_FAIL
+            });
+        }
+
+    }catch{
+        dispatch({
+                type: GET_BLOG_LIST_CATEGORIES_FAIL
+            });
+    }
+}
+
+export const get_blog_list_category_page = (category_id,p) => async dispatch => {
+
+    const config = {
+        headers: {
+            'Accept': 'application/json'
+        }
+    };
+
+    try{
+        const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/blog/category/${category_id}/?p=${p}`, config)
+
+
+        if (res.status === 200){
+            dispatch({
+                type: GET_BLOG_LIST_CATEGORIES_SUCCESS,
+                payload: res.data
+            });
+        } else {
+            dispatch({
+                type: GET_BLOG_LIST_CATEGORIES_FAIL
+            });
+        }
+
+    }catch{
+        dispatch({
+                type: GET_BLOG_LIST_CATEGORIES_FAIL
             });
     }
 }
